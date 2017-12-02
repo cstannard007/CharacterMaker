77c63c3bb8c4cd58ec6d3d440b341fad05f343d8
Christopher Stannard <32204226+cstannard007@users.noreply.github.com>
2017-12-02 16:12:57 -0500

    update

--- Character.java
+++ Character.java
@@ -3,7 +3,10 @@ import java.util.*;
 public class Character{
 
   String name;
+  String[] ability = new String[] {"Str", "Dex", "Con", "Int", "Wis", "Cha"};
   int[] abilityStats = new int [6];
+  int[] totalAS = new int [6];
+  int[] modifiers = new int [6];
 
   public void getName(Scanner console){
     this.name = console.next();
@@ -11,7 +14,7 @@ public class Character{
 
   public void randomAbilityStats(){
     for (int i = 0; i < this.abilityStats.length; i++) {
-      abilityStats[i] = randomStatRoller();
+      this.abilityStats[i] = randomStatRoller();
     }
   }
 
@@ -29,4 +32,5 @@ public class Character{
     }
     return sum;
   }
+
 }
--- Elf.java
+++ Elf.java
@@ -11,11 +11,10 @@ public class Elf{
   // Array for skill prof.
   boolean[] skill = new boolean[18];
   // Racial abilities
-  String special1 = "Fey Ancestry: You have advantage on saving throws against being charmed, and magic can’t put you to sleep.";
-  String special2 = "Trance: Elves don’t need to sleep. Instead, they meditate deeply, remaining semiconscious, for 4 hours a day. (The Common word for such meditation is \"trance.\") While meditating, you can dream after a fashion; such dreams are actually mental exercises that have become reflexive through years of practice. After resting in this way, you gain the same benefit that a human does from 8 hours of sleep.";
+  String special = "Fey Ancestry: You have advantage on saving throws against being charmed, and magic can't put you to sleep.\n" +
+                    "Trance: Elves don’t need to sleep. Instead, they meditate deeply, remaining semiconscious, for 4 hours a day. (The Common word for such meditation is \"trance.\") While meditating, you can dream after a fashion; such dreams are actually mental exercises that have become reflexive through years of practice. After resting in this way, you gain the same benefit that a human does from 8 hours of sleep.";
   // Race languages
-  String lang1 = "Common";
-  String lang2 = "Elven";
+  String lang = "Common, Elven";
 
   public Elf(){
     // skill prof. Perception
@@ -26,16 +25,16 @@ public class Elf{
 
 // subrace High Elf
 class SubRaceHighElf extends Elf{
-
-  int[] SubStats = new int[] {0, 0, 0, 1, 0, 0};
-  String subSpecial1 = "Cantrip: You know one cantrip of your choice from the wizard spell list. Intelligence is your spellcasting ability for it.";
+  String race = "High Elf";
+  int[] subStats = new int[] {0, 0, 0, 1, 0, 0};
+  String subSpecial = "Cantrip: You know one cantrip of your choice from the wizard spell list. Intelligence is your spellcasting ability for it.";
   String langBonus;
 }
 
 // subrace Wood Elf
 class SubRaceWoodElf extends Elf{
-
-  int[] SubStats = new int[] {0, 0, 0, 0, 1, 0};
+  String race = "Wood Elf";
+  int[] subStats = new int[] {0, 0, 0, 0, 1, 0};
   String subSpecial1 = "Elf Weapon Training: You have proficiency with the longsword, shortsword, shortbow, and longbow.";
   String subSpecial2 = "Mask of the Wild: You can attempt to hide even when you are only lightly obscured by foliage, heavy rain, falling snow, mist, and other natural phenomena.";
   int speed = 35;
@@ -45,7 +44,7 @@ class SubRaceWoodElf extends Elf{
 // subrace Eladrin
 class SubRaceEladrin extends Elf{
 
-  int[] SubStats = new int[] {0, 0, 0, 1, 0, 0};
+  int[] subStats = new int[] {0, 0, 0, 1, 0, 0};
   String subSpecial1 = "Elf Weapon Training: You have proficiency with the longsword, shortsword, shortbow, and longbow.";
   String subSpecial2 = " Fey Step: You can cast the misty step spell once using this trait. You regain the ability to do so when you finish a short or long rest.";
 }
