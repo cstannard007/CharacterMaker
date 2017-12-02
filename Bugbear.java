7a70de8799fa0aea0cd051bca3cd5e41a58f8e6d
Patrick Nason <nasonp201@potsdam.edu>
2017-12-02 12:54:35 -0500

    Bugbear Race Library

---
+++ Bugbear.java
@@ -0,0 +1,23 @@
+// main Race
+public class Bugbear {
+    // Array for stat modifications (Str, Dex, Con, Int, Wis, Cha)
+    int[] stats = new int[] {2, 1, 0, 0, 0, 0};
+    // minimum Age
+    int age = 80;
+    // Race alignment
+    String alignment = "Chaotic evil";
+    // Race size
+    String size = "Medium";
+    // Race speed
+    int speed = 30;
+    // Race Vision
+    String vision = "Darkvision: 60 feet";
+    // Special abilities
+    String special1 = "Long-Limbed: When you make a melee attack on your turn, your reach for it is 5 feet greater than normal";
+    String special2 = "Powerful Build: You count as one size larger when determining your carrying capacity and the weight you can push, drag, or lift";
+    String special3 = "Sneaky: You are proficient in the Stealth skill";
+    String special4 = "Surprise Attack: If you surprise a creature and hit it with an attack on your first turn in combat, the attack deals an extra 2d6 damage to it. You can use this trait only once per combat";
+    // languages
+    String lang1 = "Common";
+    String lang2 = "Goblin";
+}
