a5bf4bf75448503e1629bd6ca36544c0cefa5a5b
Christopher Stannard <32204226+cstannard007@users.noreply.github.com>
2017-12-02 13:34:44 -0500

    Elf and updated aasimar

---
+++ RaceElf.java
@@ -0,0 +1,20 @@
+public class RaceElf{
+
+  int[] stat1 = new int[6];
+  int speed;
+  String size;
+  boolean[] skill = new boolean[18];
+  String racialAbilities;
+  String languages;
+
+  public RaceElf(){
+    this.stat1[1] = 2;
+    this.speed = 30;
+    this.size = "Medium";
+    this.skill[12] = true;
+    this.racialAbilities = "Darkvision \nFey Ancestry \nTrance";
+    this.languages = "Languages: Common, Elvish";
+
+  }
+
+}
