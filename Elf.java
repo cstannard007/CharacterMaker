331039bc68e583468dc7d2ec62f5f48ca1924c41
Christopher Stannard <32204226+cstannard007@users.noreply.github.com>
2017-12-02 18:10:15 -0500

    elf update

--- Elf.java
+++ Elf.java
@@ -12,7 +12,7 @@ public class Elf{
   boolean[] skill = new boolean[18];
   // Racial abilities
   String special = "Fey Ancestry: You have advantage on saving throws against being charmed, and magic can't put you to sleep.\n\n" +
-                    "Trance: Elves don’t need to sleep. Instead, they meditate deeply, remaining semiconscious, for 4 hours a day. (The Common word for such meditation is \"trance.\") While meditating, you can dream after a fashion; such dreams are actually mental exercises that have become reflexive through years of practice. After resting in this way, you gain the same benefit that a human does from 8 hours of sleep.\n";
+                    "Trance: Elves don't need to sleep. Instead, they meditate deeply, remaining semiconscious, for 4 hours a day. (The Common word for such meditation is \"trance.\") While meditating, you can dream after a fashion; such dreams are actually mental exercises that have become reflexive through years of practice. After resting in this way, you gain the same benefit that a human does from 8 hours of sleep.\n";
   // Race languages
   String lang = "Common, Elven";
 
@@ -35,15 +35,15 @@ class SubRaceHighElf extends Elf{
 class SubRaceWoodElf extends Elf{
   String race = "Wood Elf";
   int[] subStats = new int[] {0, 0, 0, 0, 1, 0};
-  String subSpecial1 = "Elf Weapon Training: You have proficiency with the longsword, shortsword, shortbow, and longbow.";
-  String subSpecial2 = "Mask of the Wild: You can attempt to hide even when you are only lightly obscured by foliage, heavy rain, falling snow, mist, and other natural phenomena.";
+  String subSpecial = "Elf Weapon Training: You have proficiency with the longsword, shortsword, shortbow, and longbow.\n\n" +
+                       "Mask of the Wild: You can attempt to hide even when you are only lightly obscured by foliage, heavy rain, falling snow, mist, and other natural phenomena.\n";
   int speed = 35;
 
 }
 
 // subrace Eladrin
 class SubRaceEladrin extends Elf{
-
+  String race = "Eladrin";
   int[] subStats = new int[] {0, 0, 0, 1, 0, 0};
   String subSpecial1 = "Elf Weapon Training: You have proficiency with the longsword, shortsword, shortbow, and longbow.";
   String subSpecial2 = " Fey Step: You can cast the misty step spell once using this trait. You regain the ability to do so when you finish a short or long rest.";
