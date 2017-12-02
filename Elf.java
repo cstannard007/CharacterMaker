ff384765a458504eab250278e0aa964da6cfbcda
Christopher Stannard <32204226+cstannard007@users.noreply.github.com>
2017-12-02 13:42:51 -0500

    elf update

--- Elf.java
+++ Elf.java
@@ -1,21 +1,30 @@
 public class Elf{
 
+  // Array for stat modifications (Str, Dex, Con, Int, Wis, Cha)
   int[] stats = new int[] {0, 2, 0, 0, 0, 0};
+  // Race size
   String size = "Medium";
+  // Race speed
   int speed = 30;
+  // Race vision
   String vision = "Darkvision: 60 feet";
+  // Array for skill prof.
   boolean[] skill = new boolean[18];
+  // Racial abilities
   String special1 = "Fey Ancestry: You have advantage on saving throws against being charmed, and magic can’t put you to sleep.";
   String special2 = "Trance: Elves don’t need to sleep. Instead, they meditate deeply, remaining semiconscious, for 4 hours a day. (The Common word for such meditation is \"trance.\") While meditating, you can dream after a fashion; such dreams are actually mental exercises that have become reflexive through years of practice. After resting in this way, you gain the same benefit that a human does from 8 hours of sleep.";
+  // Race languages
   String lang1 = "Common";
   String lang2 = "Elven";
 
   public Elf(){
+    // skill prof. Perception
     skill[12] = true;
   }
 
 }
 
+// subrace High Elf
 class SubRaceHighElf extends Elf{
 
   int[] SubStats = new int[] {0, 0, 0, 1, 0, 0};
@@ -23,6 +32,7 @@ class SubRaceHighElf extends Elf{
   String langBonus;
 }
 
+// subrace Wood Elf
 class SubRaceWoodElf extends Elf{
 
   int[] SubStats = new int[] {0, 0, 0, 0, 1, 0};
@@ -32,6 +42,7 @@ class SubRaceWoodElf extends Elf{
 
 }
 
+// subrace Eladrin
 class SubRaceEladrin extends Elf{
 
   int[] SubStats = new int[] {0, 0, 0, 1, 0, 0};
